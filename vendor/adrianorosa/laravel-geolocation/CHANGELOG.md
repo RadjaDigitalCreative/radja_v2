f95207c55978ac18970bef1e88be4e676a2e55bd
radjadigital <wendiardiawanhappy@gmail.com>
2021-07-27 02:18:44 +0700

    pertama aja

---
+++ vendor/adrianorosa/laravel-geolocation/CHANGELOG.md
@@ -0,0 +1,28 @@
+CHANGELOG
+=========
+
+### v0.3.0
+
+- 2019-11-15  - update requirement version constrain for laravel 7.0 and 8.0
+
+### v0.2.0
+
+- 2019-11-15  - update requirement version constrain for laravel 6.*
+
+### v0.1.3
+
+- 2019-08-29 - Removed DeferrableProvider interface, now the package is not deferred by default
+- 2019-08-29 - Add new Facade `GeoLocation::countries` method to get the translation list of countries codes
+
+### v0.1.2
+
+ - 2019-08-28 - Add TravisCI
+ - 2019-08-28 - Add new method GeoLocationDetails::getCountryCode()
+ 
+ ### v0.1.1
+
+ - 2019-08-22 - Add support for laravel 6.0
+ 
+### v0.1.0
+
+ - 2019-08-13 - Initial Release
---
+++ vendor/adrianorosa/laravel-geolocation/tests/GeoLocationTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Adrianorosa\GeoLocation\Tests;
+
+use Adrianorosa\GeoLocation\GeoLocation;
+use Adrianorosa\GeoLocation\GeoLocationDetails;
+use Adrianorosa\GeoLocation\GeoLocationException;
+
+class GeoLocationTest extends TestCase
+{
+    public function testLoadConfig()
+    {
+        $this->assertNotNull($this->app['config']['geolocation']);
+    }
+
+    public function testProviderBoundInstance()
+    {
+        $this->assertTrue($this->app->bound('geolocation'));
+    }
+
+    public function testProvider()
+    {
+        $this->assertTrue($this->app->bound('geolocation'));
+    }
+
+    public function testGeoLocation()
+    {
+        /**@var \Illuminate\Cache\ArrayStore $cache*/
+        $cache = $this->app->get('cache')->getStore();
+        $ipAddress = '8.8.8.8';
+        $data = json_decode('{"ip":"' . $ipAddress . '","city":"Mountain View","region":"California","country":"US","loc":"37.3860,-122.0838"}', true);
+        $cache->put($ipAddress, $data, 2000);
+
+        $this->app->setLocale('en');
+
+        $info = GeoLocation::lookup($ipAddress);
+
+        $this->assertInstanceOf(GeoLocationDetails::class, $info);
+        $this->assertEquals($ipAddress, $info->getIp());
+        $this->assertEquals('Mountain View', $info->getCity());
+        $this->assertEquals('California', $info->getRegion());
+        $this->assertEquals('United States', $info->getCountry());
+        $this->assertEquals('US', $info->getCountryCode());
+        $this->assertIsFloat($info->getLatitude());
+        $this->assertIsFloat($info->getLongitude());
+    }
+
+    public function testCountryLocaleChange()
+    {
+        /**@var \Illuminate\Cache\ArrayStore $cache*/
+        $cache = $this->app->get('cache')->getStore();
+        $ipAddress = '8.8.8.8';
+        $data = json_decode('{"ip":"' . $ipAddress . '","city":"Mountain View","region":"California","country":"US","loc":"37.3860,-122.0838"}', true);
+        $cache->put($ipAddress, $data, 2000);
+
+        $this->app->setLocale('pt_BR');
+
+        $info = GeoLocation::lookup($ipAddress);
+
+        $this->assertInstanceOf(GeoLocationDetails::class, $info);
+        $this->assertEquals('Estados Unidos', $info->getCountry());
+        $this->assertEquals('US', $info->getCountryCode());
+    }
+
+    public function testGelLocationException()
+    {
+        $this->app->setLocale('pt_BR');
+
+        $this->expectException(GeoLocationException::class);
+        GeoLocation::lookup('1222398333');
+    }
+
+    public function testGetTranslationsOfCountriesLocale()
+    {
+        $en = GeoLocation::countries('en');
+        $this->assertArrayHasKey('US', $en);
+        $this->assertEquals('United States', $en['US']);
+
+        $pt_BR = GeoLocation::countries('pt_BR');
+        $this->assertArrayHasKey('US', $pt_BR);
+        $this->assertEquals('Estados Unidos', $pt_BR['US']);
+    }
+}
